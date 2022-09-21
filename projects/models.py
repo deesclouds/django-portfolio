49c0442d1ce6e96b853cf7e77101787b93d9512c
Darnell Davis <deescloud@protonmail.com>
2022-09-20 18:50:51 -0700

    first upload of files

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.TextField()
+    image = models.FilePathField(path="/img")
+
+    
---
+++ projects/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from projects.models import Project
+# Index view shows snippet of information about each project
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+# Detail view that shows more information on a particular topic
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
